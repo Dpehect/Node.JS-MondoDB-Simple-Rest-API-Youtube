69769db77808aaccc3e60de932e84590fdcee19a
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2023-01-15 02:28:50 +0300

    Add files via upload

---
+++ insertmany.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb+srv://Dpehect:geçicişifre@cluster0.olfqcop.mongodb.net/test"
+
+const client = new MongoClient(url,{useNewUrlParser:true})
+
+client.connect(err=>{
+    const ilkveritabanim = client.db('ilkveritabani')
+    const koleksiyon = ilkveritabanim.collection('kullanicilar')
+    var veri = [{isim:"Mauro",soyisim:"Icardi",yas:"29"},{isim:"Yunus Emre",soyisim:"Gürlek",yas:"20"},{isim:"Yunus",soyisim:"Akgün",yas:"22"}]
+    koleksiyon.insertMany(veri,function(err,res){
+        console.log(res)
+        client.close()
+    })
+})
